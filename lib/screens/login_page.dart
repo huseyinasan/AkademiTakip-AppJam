eab77025846e0a05c2b4999f14a61d15aa09ef91
caglaerkalkan <erkalkancagla@gmail.com>
2023-04-08 12:02:44 +0300

    log in assetleri ve log in sayfası

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:appjam/screens/ana_panel.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(MyApp());
@@ -22,59 +23,105 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final _formKey = GlobalKey<FormState>();
 
-  String _username = '';
-  String _password = '';
+  String _username = 'kullaniciadi';
+  String _password = 'sifre';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Giriş Sayfası'),
+        backgroundColor: Colors.grey,
+        elevation: 0,
+        title: Text(
+          'Oyun ve Uygulama Akademisi',
+          style: TextStyle(color: Colors.black),
+        ),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Kullanıcı Adı',
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/logo.png',
+                  width: 150,
+                  height: 150,
                 ),
-                validator: (value) {
-                  if (value?.isEmpty ?? true) {
-                    return 'Please enter your username';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  _username = value ?? '';
-                },
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Şifre',
+                SizedBox(height: 20),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Kullanıcı Adı',
+                  ),
+                  validator: (value) {
+                    if (value?.isEmpty ?? true) {
+                      return 'Lütfen kullanıcı adınızı girin';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _username = value ?? '';
+                  },
                 ),
-                obscureText: true,
-                validator: (value) {
-                  if (value?.isEmpty ?? true) {
-                    return 'Please enter your username';
-                  }
-                  return null;
-                },
-                onSaved: (value) {
-                  _username = value ?? '';
-                },
-              ),
-              SizedBox(height: 16.0),
-              ElevatedButton(
-                child: Text('Giriş Yap'),
-                onPressed: () {
-                  _navigatePanel(context);
-                },
-              ),
-            ],
+                SizedBox(height: 10),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Şifre',
+                  ),
+                  obscureText: true,
+                  validator: (value) {
+                    if (value?.isEmpty ?? true) {
+                      return 'Lütfen şifrenizi girin';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _password = value ?? '';
+                  },
+                ),
+                SizedBox(height: 16.0),
+                ElevatedButton(
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: Text(
+                      'Giriş Yap',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 20.0, color: Colors.white),
+                    ),
+                  ),
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      _formKey.currentState!.save();
+                      if (_username == 'kullaniciadi' && _password == 'sifre') {
+                        _navigatePanel(context);
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text('Kullanıcı adı veya şifre hatalı'),
+                          ),
+                        );
+                      }
+                    }
+                  },
+
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.red,
+                  ),
+                ),
+                SizedBox(height: 8.0),
+                TextButton(
+                  child: Text(
+                    'Şifrenizi mi unuttunuz?',
+                    style: TextStyle(
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                  onPressed: () {},
+                ),
+              ],
+            ),
           ),
         ),
       ),
